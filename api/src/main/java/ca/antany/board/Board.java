0c6afa4e498896b361e81a8429128c91b7e166a8
antany <antany84@gmail.com>
2020-09-06 09:59:25 -0400

    Initial Setup of Board Application

---
+++ api/src/main/java/ca/antany/board/Board.java
@@ -0,0 +1,13 @@
+package ca.antany.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board.class, args);
+	}
+
+}
